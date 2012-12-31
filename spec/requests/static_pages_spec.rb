cd5aa8b837fd5c045ef5f5f9154e78d51aba78fd
tborres <tborres@base2.io>
2012-12-31 11:15:51 -0500

    Remove custom title from home page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,15 @@ describe "Home page" do
     page.should have_selector('h1', :text => 'Sample App')
   end
 
-  it "should have the right title" do
+  it "should have the base title" do
     visit '/static_pages/home'
-    page.should have_selector('title', :text => " | Home")
+    page.should have_selector('title',
+                              :text => "Ruby on Rails Tutorial Sample App")
+  end
+
+  it "should not have a custom page title" do
+    visit '/static_pages/home'
+    page.should_not have_selector('title', :text => '| Home')
   end
 end
 
@@ -22,7 +28,7 @@ describe "Help page" do
 
   it "should have the right title" do
     visit '/static_pages/help'
-    page.should have_selector('title', :text => " | Help")
+    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
   end
 end
 
@@ -35,6 +41,6 @@ describe "About page" do
 
   it "should have the right title" do
     visit '/static_pages/about'
-    page.should have_selector('title', :text => " | About Us")
+    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
   end
 end
